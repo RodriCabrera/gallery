c33c4db80a830acbd0770d6c575da3d503b655f8
Rodrigo <rodrigo1cabrera2@gmail.com>
2021-09-05 21:49:01 -0300

    Add: Greeting

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './ItemListContainer.css'
 
-const ItemListContainer = () => {
+const ItemListContainer = (props) => {
     return(
         <div className="catalogo">
+            <h3>{props.greeting}</h3>
             <p>Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo Catálogo </p>
         </div>
     )
