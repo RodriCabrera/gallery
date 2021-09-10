8ee6775c3f7bd59a05f8aa74d6e4810600d401bd
Rodrigo <rodrigo1cabrera2@gmail.com>
2021-09-10 20:31:30 -0300

    Working card display with data from the Cleveland Museum API

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import "./Card.css"
+import React from "react";
+import "./Card.css";
 import ItemCount from "../../components/ItemCount/ItemCount";
 
+const Card = (props) => {
+	return (
+		<div className="card">
+			<img className="card-img" src={props.image} alt="" />
+			<h4 className="card-title">{props.title}</h4>
+			<h6 className="card-author">{props.author}</h6>
+			<ItemCount />
+		</div>
+	);
+};
 
-const Card = ({name = "card name", price = "$$$"}) => {
-    return (
-        <div className="card">
-            <img className="card-img" src="https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder.png" alt=""/>
-            <h4 className="card-name">{name}</h4>
-            <h5 className="card-price">{price}</h5>
-            <ItemCount />
-        </div>
-    )
-}
-
-export default Card
+export default Card;
--- src/containers/ItemListContainer/ItemListContainer.js
+++ src/containers/ItemListContainer/ItemListContainer.js
@@ -1,17 +1,58 @@
 import React from "react";
 import "./ItemListContainer.css";
 import Card from "../../components/Card/Card";
+import { useState, useEffect } from "react";
 
 const ItemListContainer = (props) => {
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		try {
+			fetch(
+				// "https://openaccess-api.clevelandart.org/api/artworks/?has_image=1&limit=8"
+				// "https://openaccess-api.clevelandart.org/api/artworks/?has_image=1&culture=andes&limit=10"
+				"https://openaccess-api.clevelandart.org/api/artworks/?has_image=1&department=Art+of+the+Americas&limit=10&indent=1"
+			)
+				.then((res) => res.json())
+				.then((result) => setItems(result.data));
+		} catch (error) {
+			console.log(error, "Falló requesto a API.");
+		}
+	}, []);
+	console.log(items);
+
+	const cardMap = () => {
+		return items.map((item) => (
+			<Card
+				title={item.title}
+				author={item.culture[0]}
+				image={item.images.web.url}
+			/>
+		));
+	};
+
 	return (
 		<div id="artworks">
 			<h2>{props.greeting}</h2>
 			<div className="itemList-wrapper">
-                <Card />
-                <Card />
-                <Card />
-				
-            </div>
+				{cardMap()}
+
+				{/* {items
+					? items.map((item) => (
+							<Card
+								title={item.title}
+								author={item.creators[0].description}
+								image={item.images.web.url}
+							/>
+					  ))
+					: "Error"} */}
+
+				{/* <Card
+					title={items[0].title}
+					author={items[0].creators[0].description}
+					image={items[0].images.web.url}
+				/> */}
+			</div>
 		</div>
 	);
 };
