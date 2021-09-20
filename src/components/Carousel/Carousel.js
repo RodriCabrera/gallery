99718d9144411cff771ba2554db94595f296a722
Rodrigo <rodrigo1cabrera2@gmail.com>
2021-09-20 11:43:32 -0300

    text mod

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,24 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Carousel.css";
 import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Carrousel = () => {
-	const [data, setData] = useState({});
-	const [loading, setLoading] = useState(false);
-
-	useEffect(() => {
-		setLoading(true);
-		const url = `https://openaccess-api.clevelandart.org/api/artworks/?has_image=1&department=Art+of+the+Americas&limit=3&indent=1&offset=${Math.round(
-			Math.random() * (200 - 30) + 30
-		)}`;
-		fetch(url)
-			.then((res) => res.json())
-			.then((res) => setData(res))
-			.catch((err) => console.log("Error:", err))
-			.finally(() => setLoading(false));
-	}, []);
-
 	return (
 		<div className="wrapper">
 			<Carousel>
@@ -32,7 +17,7 @@ const Carrousel = () => {
 					</h2>
 				</Carousel.Item>
 				<Carousel.Item className="banner banner3">
-					<h2 className="banner-header">Artist or collector, contact us.</h2>
+					<h2 className="banner-header">Please feel free to contact us.</h2>
 				</Carousel.Item>
 			</Carousel>
 		</div>
