7da5d846b6e5e410709d245bebc5e2971eb3fd4b
Rodrigo <rodrigo1cabrera2@gmail.com>
2021-10-09 13:41:24 -0300

    Page not found add icon

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { GiBoatFishing } from "react-icons/gi";
 
 const NotFound = () => {
 	return (
-		<div>
-			<h2>Page not found.</h2>
+		<div
+			style={{
+				height: "70vh",
+				padding: "2rem",
+				display: "flex",
+				flexDirection: "column",
+			}}
+		>
+			<h2 style={{ textAlign: "center" }}>Page not found</h2>
+			<GiBoatFishing style={{ fontSize: "5rem", textAlign: "center" }} />
 		</div>
 	);
 };
