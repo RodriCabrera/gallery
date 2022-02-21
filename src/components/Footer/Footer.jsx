a48b855ef91402acf0c7bea7eaccf23f4072cc73
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-02-20 21:31:03 -0300

    change year

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { Container, Text, TextBox, Wrapper } from "./Footer.elements";
 
 const Footer = () => {
-	return (
-		<div>
-			<Container>
-				<Wrapper>
-					<TextBox>
-						<Text>ART LATIN AMERICA © 2021</Text>
-						<Text>Website by Rodrigo Cabrera</Text>
-					</TextBox>
-				</Wrapper>
-			</Container>
-		</div>
-	);
+  return (
+    <div>
+      <Container>
+        <Wrapper>
+          <TextBox>
+            <Text>ART LATIN AMERICA © 2022</Text>
+            <Text>Website by Rodrigo Cabrera</Text>
+          </TextBox>
+        </Wrapper>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
