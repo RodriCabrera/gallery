da36160376a36f3aa9f08560d87d295cd9495bc2
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-02-20 21:15:57 -0300

    Get carrousel data from firebase

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,27 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Carousel.css";
 import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { getFirestore } from "../../firebase";
 
 const Carrousel = () => {
-	return (
-		<>
-			<Carousel className="wrapper">
-				<Carousel.Item className="banner banner1">
-					<h2 className="banner-header">Welcome to Art Latin America</h2>
-				</Carousel.Item>
-				<Carousel.Item className="banner banner2">
-					<h2 className="banner-header">
-						Art Latin America's mission is to be a direct nexus between Artists
-						and Collectors.
-					</h2>
-				</Carousel.Item>
-				<Carousel.Item className="banner banner3">
-					<h2 className="banner-header">Please feel free to contact us.</h2>
-				</Carousel.Item>
-			</Carousel>
-		</>
-	);
+  const [banners, setBanners] = useState();
+
+  useEffect(() => {
+    const db = getFirestore();
+    db.collection("banners")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          setBanners(
+            querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+          );
+        });
+      });
+  }, []);
+  console.log(banners, "BANNERS");
+
+  const getBannerLink = (id) => {
+    return banners?.find((banner) => banner.id === id).link;
+  };
+  const getBannerText = (id) => {
+    return banners?.find((banner) => banner.id === id).text;
+  };
+  console.log(getBannerLink(1));
+  return (
+    <>
+      <Carousel className="wrapper">
+        <Carousel.Item
+          className="banner banner1"
+          style={{ backgroundImage: `url(${getBannerLink(1)})` }}
+        >
+          <h2 className="banner-header">{getBannerText(1)}</h2>
+        </Carousel.Item>
+        <Carousel.Item
+          className="banner"
+          style={{ backgroundImage: `url(${getBannerLink(2)})` }}
+        >
+          <h2 className="banner-header">{getBannerText(2)}</h2>
+        </Carousel.Item>
+        <Carousel.Item
+          className="banner"
+          style={{ backgroundImage: `url(${getBannerLink(3)})` }}
+        >
+          <h2 className="banner-header">{getBannerText(3)}</h2>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
 };
 
 export default Carrousel;
