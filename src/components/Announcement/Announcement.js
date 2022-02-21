218f0415e35f1630820036ec8e6203c4d904b2bd
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-02-20 21:15:36 -0300

    Add announcement

---
+++ src/components/Announcement/Announcement.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./announcement.css";
+
+const Announcement = () => {
+  return (
+    <div className="acontainer">
+      <h3>25.05.22 - 7.06.22</h3>
+      <h3>ARTISTES ARGENTINS CONTEMPORAINS</h3>
+      <p>
+        Galerie de l'Ambassade d'Argentine à Paris, 6 Rue Cimarosa, 75116 Paris
+      </p>
+      <p>Vernissage: 25 mai 2022 à 18 hs</p>
+    </div>
+  );
+};
+
+export default Announcement;
